bf68214e5bcec5c22bce7f0bdfec45b82123d20d
BTAO TEAM <36434065+VijayanagaramRahul@users.noreply.github.com>
2020-12-07 10:27:38 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysqli_connect("localhost","id10927729_bcbuildingappbc","bcbuildingappbc","id10927729_bcbuildingappbc");
+$conn=mysqli_connect("localhost","DB_USERNAME","PASSWORD","DB_NAME");
 
 
 
